8dc636b45d9c3227d16d9c574e967acd7aa95f65
Rob Martin <rob@version2beta.com>
2015-09-18 17:34:14 -0600

    full Cheater with docs

--- an_ounce_of_elixir/wwfcheater/cheater.exs
+++ an_ounce_of_elixir/wwfcheater/cheater.exs
@@ -1,10 +1,56 @@
 defmodule Cheater do
+  @moduledoc """
+  Find words that can be made from a jumbled collection
+  of letters.
+    ```
+    iex> words = Cheater.make_dict "words.txt"
+    iex> Cheater.find('uljemb', words)
+    ['be', 'bel', 'blue', 'blume', ...]
+    ```
+  """
 
-  def pick([], _), do: []
-  def pick([head | tail], elt) when head == elt, do: tail
-  def pick([head | tail], [elt]) when head == elt, do: tail
-  def pick([head | tail], elt), do: [head] ++ pick(tail, elt)
+  @doc """
+  Find the intersection between a dictionary word and a
+  set of tiles.
+    `pick('mujleb', 'jumble') == 'jumble'`
+  """
+  def pick(tiles, word), do: pick(tiles, word, '')
+  def pick(tiles, word, int) when tiles == '' or word == '' do
+    int
+  end
+  def pick(tiles, [char | chars], int) do
+    cond do
+      Enum.member? tiles, char ->
+        pick(tiles -- [char], chars, int ++ [char])
+      true ->
+        pick(tiles, chars, int)
+    end
+  end
 
+  @doc """
+  Test whether a word can be made from a set of tiles.
+    `match('mujleb', 'jumble') == true`
+  """
+  def match(tiles, word) do
+    word == pick(tiles, word)
+  end
+
+  @doc """
+  Returns a list of char_lists from a file containing words,
+  one word per line.
+    `words = make_dict("words.txt")`
+  """
+  def make_dict(file) do
+    {:ok, dict} = File.read(file)
+    dict |> String.split("\n") |>
+      Enum.map fn word ->
+        to_char_list word
+      end
+  end
+
+  def find(tiles, words) do
+    Enum.filter words, fn word -> match tiles, word end
+  end
 end
 
 
@@ -14,12 +60,37 @@ defmodule CheaterTest do
   use ExUnit.Case
 
   test "pick" do
-    assert MyList.pick([], nil) == []
-    assert MyList.pick([1], 1) == []
-    assert MyList.pick([1], 2) == [1]
-    assert MyList.pick([0, 1, 2], 1) == [0, 2]
-    assert MyList.pick([0, 1, 1, 2], 1) == [0, 1, 2]
-    assert MyList.pick('abcde', 'c') == 'abde'
+    assert Cheater.pick('', '') == ''
+    assert Cheater.pick('abc', '') == ''
+    assert Cheater.pick('abc', 'd') == ''
+    assert Cheater.pick('abc', 'b') == 'b'
+    assert Cheater.pick('abcdef', 'bdf') == 'bdf'
+    assert Cheater.pick('abcabc', 'abc') == 'abc'
+    assert Cheater.pick('abc', 'abcabc') == 'abc'
+    assert Cheater.pick('ujmleb', 'jumble') == 'jumble'
+  end
+
+  test "match" do
+    assert Cheater.match('', '') == true
+    assert Cheater.match('abc', 'abc') == true
+    assert Cheater.match('abc', '') == true
+    assert Cheater.match('', 'abc') == false
+    assert Cheater.match('abc', 'def') == false
+    assert Cheater.match('ujmleb', 'jumble') == true
+  end
+
+  test "dict" do
+    [word | words] = Cheater.make_dict "words.txt"
+    assert is_list words
+    assert is_list word
+  end
+
+  test "find" do
+    words = Cheater.make_dict("words.txt")
+    matches = Cheater.find('ujmebl', words)
+    assert Enum.member? matches, 'jumble'
+    refute Enum.member? matches, 'mumble'
+    refute Enum.member? matches, 'bell'
   end
 
 end
